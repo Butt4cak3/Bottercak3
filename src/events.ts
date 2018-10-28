cbb2367ccbfb78657b11e3b4b9ae4d55fd08fb61
Marius Becker <marius.becker.8@gmail.com>
2018-10-28 17:12:50 +0100

    Catch exceptions thrown by event handlers

--- src/events.ts
+++ src/events.ts
@@ -1,27 +1,43 @@
 import { ChatMessage } from "./connector";
 
-type Handler<T> = (data: T) => void;
+type Handler<T, R> = (data: T) => R;
 
-interface Listener<T> {
-  handler: Handler<T>;
+interface Listener<T, R> {
+  handler: Handler<T, R>;
   thisArg: any;
 }
 
-export class Event<T> {
-  private listeners = new Map<Handler<T>, Listener<T>>();
+export class Event<T, R = void> {
+  private listeners = new Map<Handler<T, R>, Listener<T, R>>();
 
-  public subscribe(handler: Handler<T>, thisArg?: any) {
+  public subscribe(handler: Handler<T, R>, thisArg?: any) {
     this.listeners.set(handler, { handler, thisArg });
   }
 
-  public unsubscribe(handler: Handler<T>) {
+  public unsubscribe(handler: Handler<T, R>) {
     this.listeners.delete(handler);
   }
 
   public invoke(data: T) {
+    const exceptions = new Map<Handler<T, R>, Error>();
+    const results = new Map<Handler<T, R>, R>();
+
     for (const listener of this.listeners.values()) {
-      listener.handler.call(listener.thisArg, data);
+      const { handler, thisArg } = listener;
+
+      try {
+        results.set(handler, handler.call(thisArg, data));
+      } catch (e) {
+        if (e instanceof Error) {
+          exceptions.set(handler, e);
+        }
+      }
     }
+
+    return {
+      results,
+      exceptions
+    };
   }
 
   public connect(event: Event<T>, convert?: (data: T) => T): void;
